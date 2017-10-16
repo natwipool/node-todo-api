81b33076b5bf7159f7750412baddef7338949383
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 21:09:53 +0700

    Fix connection mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://todoapp:123454321@ds121345.mlab.com:21345/todo-app'
-}
+};
 
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = { mongoose };
