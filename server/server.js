3c1e2604b10166fd555db03184c5780a2c1bdd11
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 16:03:39 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
   }).then(
     todo => {
       if (!todo) {
-        return res.status(400).send();
+        return res.status(404).send();
       }
       res.send({ todo });
     },
