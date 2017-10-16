21f087587751fb365bfbe71d4bc46722ec4ba577
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 22:24:10 +0700

    Fix mongoose connection to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let db = {
+var db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://todoapp:123454321@ds121345.mlab.com:21345/todo-app'
 };
