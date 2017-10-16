5b10b8c88a997b04981e1994a522e31d7df39f69
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 21:01:10 +0700

    Add database user on mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://natwipool:n0870726321@ds121345.mlab.com:21345/todo-app'
+  mlab: 'mongodb://todoapp:123454321@ds121345.mlab.com:21345/todo-app'
 }
 
 mongoose.connect(db.localhost || db.mlab);
