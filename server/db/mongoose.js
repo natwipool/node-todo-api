37bc21ee66258975e39920167089ff2b674d00a3
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 22:18:26 +0700

    Fix mongoose connection to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,7 @@ let db = {
   mlab: 'mongodb://todoapp:123454321@ds121345.mlab.com:21345/todo-app'
 };
 
-mongoose.connect(db.mlab || db.localhost);
+mongoose.Promise = global.Promise;
+mongoose.connect(db.localhost ? db.localhost: db.mlab, {useMongoClient: true});
 
 module.exports = { mongoose };
