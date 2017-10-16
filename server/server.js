9b048d721671e83855c364e75c9817320f99cb00
natwipool <30383412+natwipool@users.noreply.github.com>
2017-10-16 15:24:16 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/Todo');
@@ -10,24 +11,49 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  
   var todo = new Todo({
     text: req.body.text
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send({ todos });
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById({
+    _id: id
+  }).then(
+    todo => {
+      if (!todo) {
+        return res.status(400).send();
+      }
+      res.send({ todo });
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(3000, () => {
